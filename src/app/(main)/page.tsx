9fea89a868532f94e91c5ee626eed3b4ed087700
Shalev Asor <shalevasor@gmail.com>
2025-08-17 13:52:46 +0300

    connected prisma with neon db

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,3 +1,12 @@
-export default function Home() {
-  return <div>home</div>;
+import { prisma } from '@/lib/prisma'
+
+export default async function Home() {
+  const productCount = await prisma.product.count()
+  
+  return (
+    <div>
+      <h1>My Store</h1>
+      <p>Products in database: {productCount}</p>
+    </div>
+  )
 }
